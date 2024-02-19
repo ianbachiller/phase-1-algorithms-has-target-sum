012e29c98d44b1802d437071054915f8269ed902
Ian Bachiller <ianbachise@gmail.com>
2024-02-19 02:32:19 -0500

    completes has target sum lab

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-function hasTargetSum(array, target) {
+function hasTargetSum(array, target) { 
   // Write your algorithm here
+  for (let a = 0; a < array.length; a++) {
+    for (let b = a+1; b < array.length; b++) {
+      if (array[a] + array[b] === target) {
+        console.log(`${array[a]} and ${array[b]}: true`);
+        return true;
+      }
+    }
+  }
+  return false
 }
+//Problem in my own words
+  //so there should be a function called hasTargetSum that will take two types of inputs: an array (full of integers) and an integer. ;=
+  //The function should return true if there' two numbers in the array that when added will result in the integer that was given as an input.
+//Test cases
+  //array input: [1,3,6,32,12,4,3], 10
+  //Will return true because 6 and 4 when added is 10
+  //array input: [1,2,7,90,56,4,34], 10
+  //will return false because no pair will result in 10
+//
 
 /* 
   Write the Big O time complexity of your function here
@@ -9,7 +27,10 @@ function hasTargetSum(array, target) {
 /* 
   Add your pseudocode here
 */
-
+  //will use quadratic notation (O(n^2)
+    //loop through the input
+    //and for every element, add that element to the other elements
+    //if for any instance during the loops there is equal to the integer input, stop, and return 
 /*
   Add written explanation of your solution here
 */
